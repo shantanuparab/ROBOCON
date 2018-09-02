9a93694fea2148e59a1407f0ec9293288fce9702
Pratik Chowdhury <pratikc@live.co.uk>
2018-09-02 16:25:14 +0530

    Run Encoder with Motors for Manual Bot with Distance Calculation

---
+++ ManualBotEncoderMotors/Constants.h
@@ -0,0 +1,57 @@
+// Ensures this Header File is only included Once while compiling
+#pragma once
+
+// This file Stores all the Constants Involved
+// Example Pin Numbers
+// Or OMNI Wheel's Diameter
+
+// Please ensure that all NULL Statements have values
+
+namespace Pin
+{
+   // Writing Pin would expand to this
+   // For example
+   // Pin pin;
+   // and
+   // uint8_t pin;
+   // would be the same statements
+   using Pin = byte;
+
+   namespace Motor
+   {
+		// FRONT LEFT
+      const Pin DIRECTION_FL = NULL;
+      const Pin PWM_FL       = NULL;
+      // FRONT RIGHT
+		const Pin DIRECTION_FR = NULL;
+      const Pin PWM_FR       = NULL;
+		// BACK LEFT
+      const Pin DIRECTION_BL = NULL;
+      const Pin PWM_BL       = NULL;
+		// BACK RIGHT
+      const Pin DIRECTION_BR = NULL;
+      const Pin PWM_BR       = NULL;
+   } // namespace Motor
+   namespace Encoder
+   {
+      // NOTE
+      // Both PINS provided to Encoder
+      // Must be Interrupt Pins
+      // If they are not,
+      // We shall face a Performance Penalty
+      // Check List of Interrupt Pins for your board
+
+      const Pin ENCODER_A = NULL;
+      const Pin ENCODER_B = NULL;
+   } // namespace Encoder
+} // namespace Pin
+
+namespace Constants
+{
+   // Store the Omni Wheel DIAMETER here
+   const int16_t OMNI_WHEEL_DIAMETER = NULL;
+
+   // Store the Number of Pulses Per Revolution
+   // Check Data Sheet for More details
+   const int16_t ENCODER_COUNTS_PER_ROTATION = NULL;
+} // namespace Constants
---
+++ ManualBotEncoderMotors/ManualBotEncoderMotors.ino
@@ -0,0 +1,70 @@
+// This file stores the List of Constants to be used
+#include "Constants.h"
+
+// This is an Encoder.h definition
+// This Must Always be Defined Before Using Encoder.h
+// Danger:- Delete this if using MORE than ONE Interrupt Pin
+// Note:- Check Again if Multiple Interrupts Used
+#define ENCODER_OPTIMIZE_INTERRUPTS
+
+// Dare not Use Encoder without using Libraries
+// For Details about this Library Check
+// http://www.pjrc.com/teensy/td_libs_Encoder.html
+#include <Encoder.h>
+
+// Create Encoder Object
+// NOTE
+// Both PINS provided to Encoder
+// Must be Interrupt Pins
+// If they are not,
+// We shall face a Performance Penalty
+// Check List of Interrupt Pins for your board
+
+Encoder g_encoder(Pin::Encoder::ENCODER_A, Pin::Encoder::ENCODER_B);
+
+#include "MotorController.h"
+
+// FRONT LEFT
+const MotorController g_motor_fl(Pin::Motor::DIRECTION_FL, Pin::Motor::PWM_FL, LOW);
+// FRONT RIGHT
+const MotorController g_motor_fr(Pin::Motor::DIRECTION_FR, Pin::Motor::PWM_FR, HIGH);
+// BACK LEFT
+const MotorController g_motor_bl(Pin::Motor::DIRECTION_BL, Pin::Motor::PWM_BL, HIGH);
+// BACK RIGHT
+const MotorController g_motor_br(Pin::Motor::DIRECTION_BR, Pin::Motor::PWM_BR, LOW);
+
+// Stores the Previous Number of Pulses
+// Volatile ensures that this variable is not 
+// Optimised Away
+volatile int32_t g_prev_number_of_pulses = 0;
+
+// Converts Number Pulses to Distance Covered by Encoder
+// Note that the Unit of Distance will be
+// Same as Unit of Diameter
+int32_t pulsesToDistance(const int32_t p_no_of_pulses)
+{
+	// distance = (PI * Diameter)*counts / (counts per complete revolution)
+	return ((PI * Constants::OMNI_WHEEL_DIAMETER) * p_no_of_pulses) /
+		Constants::ENCODER_COUNTS_PER_ROTATION;
+}
+
+// The setup() function runs once each time the micro-controller starts
+void setup() {}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+	// Get the Encoder Current Number of Pulses Detected
+	const int32_t number_of_pulses = g_encoder.read();
+
+	// If Changed then Act
+	if (number_of_pulses != g_prev_number_of_pulses)
+	{
+      const int32_t distance_covered = pulsesToDistance(number_of_pulses);
+		Serial.print("The Object Covered\t:");
+		Serial.print(distance_covered);
+		Serial.println();
+
+	}
+
+}
