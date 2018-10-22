6b636367b57080553293a31684dcd0eebc399969
Pratik Chowdhury <pratikc@live.co.uk>
2018-10-23 04:22:50 +0530

    Added Support for C++ Pi I2C

---
+++ OpenCV/Libraries/I2CTest.cxx
@@ -0,0 +1,26 @@
+// Note that the given code must follow
+// https://learn.sparkfun.com/tutorials/raspberry-pi-spi-and-i2c-tutorial/all
+// To work Properly
+
+#include "I2C.hxx"
+
+#include <iostream>
+
+// I2C Device Address
+const std::uint8_t I2C_ADDRESS = 0x04;
+
+// I2C Device Default Register
+const std::uint8_t I2C_DATA_REGISTER = 0x01;
+
+int main()
+{
+	// Create a Pi I2C Device
+	const Pi::I2C device{I2C_ADDRESS};
+
+	// Send 255 Values
+	for (std::uint8_t i = 0; i < 255; ++i)
+	{
+		// Write a Byte to the Data Register
+		device.write(I2C_DATA_REGISTER, i);
+	}
+}
