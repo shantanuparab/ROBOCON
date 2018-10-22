8a813efe7d7aa1735c866708ccf3cd5e0e37aebe
Pratik Chowdhury <pratikc@live.co.uk>
2018-10-23 04:24:27 +0530

    Added Support for C++ Pi I2C

--- OpenCV/Libraries/I2C.hxx
+++ OpenCV/Libraries/I2C.hxx
@@ -11,64 +11,64 @@
 
 namespace Pi
 {
-   struct I2C
-   {
-    private:
-      const int m_i2c_handle;
+	struct I2C
+	{
+	private:
+		const int m_i2c_handle;
 
-    public:
-      I2C(const std::uint8_t p_dev_address) : m_i2c_handle{wiringPiI2CSetup(p_dev_address)}
-      {
-         // Returns -1 as Handle on Error
-         if (m_i2c_handle == -1)
-            throw std::runtime_error("Unable to Access I2C Device Handle");
-      }
+	public:
+		I2C(const std::uint8_t p_dev_address) : m_i2c_handle{ wiringPiI2CSetup(p_dev_address) }
+		{
+			// Returns -1 as Handle on Error
+			if (m_i2c_handle == -1)
+				throw std::runtime_error("Unable to Access I2C Device Handle");
+		}
 		~I2C()
 		{
 			// Close the I2C Handle
 			close(m_i2c_handle);
 		}
 
-      // Reads a Byte without the need of a register
-      std::uint8_t readByte() const noexcept
-      {
-         return wiringPiI2CRead(m_i2c_handle);
-      }
-      // Writes to a Byte without the need for a register
-      bool write(const std::uint8_t p_data) const noexcept
-      {
-         // Returns -1 on Failure
-         return wiringPiI2CWrite(m_i2c_handle, p_data) != -1;
-      }
-      std::uint8_t readByte(const std::uint8_t p_reg) const
-      {
-         const auto reading = wiringPiI2CReadReg8(m_i2c_handle, p_reg);
-         if (reading == -1)
-            throw std::runtime_error("Unable to Read Data from Device");
-         else
-            return static_cast<std::uint8_t>(reading);
-      }
-      std::uint16_t readData(const std::uint8_t p_reg) const
-      {
-         const auto reading = wiringPiI2CReadReg16(m_i2c_handle, p_reg);
-         if (reading == -1)
-            throw std::runtime_error("Unable to Read Data from Device");
-         else
-            return static_cast<std::uint16_t>(reading);
-      }
-      // Write Data to Specified Address
-      bool write(const std::uint8_t p_reg, const std::uint8_t p_data) const noexcept
-      {
-         return wiringPiI2CWriteReg8(m_i2c_handle, p_reg, p_data) != -1;
-      }
-      bool write(const std::uint8_t p_reg, const std::uint16_t p_data) const noexcept
-      {
-         return wiringPiI2CWriteReg16(m_i2c_handle, p_reg, p_data) != -1;
-      }
+		// Reads a Byte without the need of a register
+		std::uint8_t readByte() const noexcept
+		{
+			return wiringPiI2CRead(m_i2c_handle);
+		}
+		// Writes to a Byte without the need for a register
+		bool write(const std::uint8_t p_data) const noexcept
+		{
+			// Returns -1 on Failure
+			return wiringPiI2CWrite(m_i2c_handle, p_data) != -1;
+		}
+		std::uint8_t readByte(const std::uint8_t p_reg) const
+		{
+			const auto reading = wiringPiI2CReadReg8(m_i2c_handle, p_reg);
+			if (reading == -1)
+				throw std::runtime_error("Unable to Read Data from Device");
+			else
+				return static_cast<std::uint8_t>(reading);
+		}
+		std::uint16_t readData(const std::uint8_t p_reg) const
+		{
+			const auto reading = wiringPiI2CReadReg16(m_i2c_handle, p_reg);
+			if (reading == -1)
+				throw std::runtime_error("Unable to Read Data from Device");
+			else
+				return static_cast<std::uint16_t>(reading);
+		}
+		// Write Data to Specified Address
+		bool write(const std::uint8_t p_reg, const std::uint8_t p_data) const noexcept
+		{
+			return wiringPiI2CWriteReg8(m_i2c_handle, p_reg, p_data) != -1;
+		}
+		bool write(const std::uint8_t p_reg, const std::uint16_t p_data) const noexcept
+		{
+			return wiringPiI2CWriteReg16(m_i2c_handle, p_reg, p_data) != -1;
+		}
 
-      int getHandle()
-      {
-         return m_i2c_handle;
-      }
-   };
+		int getHandle()
+		{
+			return m_i2c_handle;
+		}
+	};
 } // namespace Pi
