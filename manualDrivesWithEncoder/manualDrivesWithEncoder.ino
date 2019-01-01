991ec38f0aba94b57ac43d748809e5e7d36a138d
Unknown <chinmay.todankar98@gmail.com>
2019-01-01 15:14:24 +0530

    Added Small Support for Turns

--- manualDrivesWithEncoder/manualDrivesWithEncoder.ino
+++ manualDrivesWithEncoder/manualDrivesWithEncoder.ino
@@ -20,6 +20,7 @@ byte lineValue;
 int setPoint = 35;
 float Kp = 1.75, Kd = 36;
 long pidTime;
+bool junction1Enab = false;
 bool junction2Enab = false;
 float lastError = 0;
 int PositionL = 0;
@@ -29,95 +30,131 @@ bool Align = false;
 
 LSA08 lineSensor1(&Serial3, baudRate, Address1, UartMode1, SerialPin1, JunctionPin1);
 LSA08 lineSensor2(&Serial3, baudRate, Address2, UartMode2, SerialPin2, JunctionPin2);
-Encoder encodR(A7 , 2);
+Encoder encodR(A7, 2);
 Encoder encodL(3, A4);
 
+struct Movements
+{
+	const byte Flag;
+	const uint32_t Dist;
+public:
+	constexpr Movements(const byte flag, const uint32_t p_dist) : Flag{ flag }, Dist{ p_dist }
+	{
+
+	}
+};
+
+PROGMEM const Movements movements[] = { Movements{1, 250}, Movements{3,10}, Movements{1,100}, Movements{2,10} };
+byte cur_idx = 0;
+
+
+void junctionCheck1()
+{
+	if (junction1Enab && flag == 1)
+	{
+		Align = true;
+		junction1Enab = false;
+	}
+}
+
+void junctionCheck2()
+{
+	if (junction2Enab && flag != 1)
+	{
+		Align = true;
+		junction2Enab = false;
+	}
+}
+
 void setup() {
-  delay(3000);
-  Serial.begin(9600);
-  Serial.println("Start Setup");
-  lineSensor1.initialize();
-  lineSensor2.initialize();
-  for (int i = 0; i < 3; i++) {
-    pinMode(motorDir[i], OUTPUT);
-    pinMode(motorPwm[i], OUTPUT);
-  }
-  Serial.println("End Setup");
-  attachInterrupt(digitalPinToInterrupt(JunctionPin1), junctionCheck, FALLING);
-  pidTime = micros();
-  movePlatform(0, 0, 0);
+	delay(3000);
+	Serial.begin(9600);
+	Serial.println("Start Setup");
+	lineSensor1.initialize();
+	lineSensor2.initialize();
+	for (int i = 0; i < 3; i++) {
+		pinMode(motorDir[i], OUTPUT);
+		pinMode(motorPwm[i], OUTPUT);
+	}
+	Serial.println("End Setup");
+	attachInterrupt(digitalPinToInterrupt(JunctionPin1), junctionCheck1, FALLING);
+	attachInterrupt(digitalPinToInterrupt(JunctionPin2), junctionCheck2, FALLING);
+	pidTime = micros();
+	movePlatform(0, 0, 0);
 }
 
 void loop() {
-  PositionL = encodL.read();
-  PositionR = encodR.read();
-  //  movePlatform(0, 100, 0);
-  //  Serial.println(PositionR);
-  //}
-  Serial.print(PositionL);
-  Serial.print("  ");
-  Serial.print(PositionR);
-  Serial.print("  ");
-  Serial.print(((PositionL + PositionR) / 2));
-  Serial.println("  ");
+	PositionL = encodL.read();
+	PositionR = encodR.read();
 
-  if (!junction2Enab && flag == 1 && abs(PositionR)  >= 250 )
-  {
-    Serial.println("HUGE");
-    junction2Enab = true;
-  }
-  if (Align)
-  {
-    Align = false;
-    encodR.write(0);
-    movePlatform(0, 50, 0);
-    while (true) {
-      PositionR = abs(encodR.read());
-      Serial.print("Encoder align");
-      Serial.println(PositionR);
-      if (PositionR > 25)
-        break;
-    }
-    movePlatform(0, 0, 0);
-  }
-  if (flag == 1)
-    lineValue = lineSensor1.read();
-  else
-    lineValue = lineSensor2.read();
+	Serial.print(PositionL);
+	Serial.print("  ");
+	Serial.print(PositionR);
+	Serial.print("  ");
+	Serial.print(((PositionL + PositionR) / 2));
+	Serial.println("  ");
 
-  if (lineValue >= 0 && lineValue <= 70 )
-  {
-    if (micros() - pidTime > 1000) {
-      Serial.print(" Line Value: ");
-      Serial.print(lineValue);
-      pidTime = micros();
-      float error = lineValue - setPoint;
-      //      Serial.print(" Error: ");
-      //      Serial.print(error);
-            float Correction = Kp * error + Kd * (error - lastError);
-      Serial.print(" flag: ");
-      Serial.println(flag);
-      if (flag == 1)
-        movePlatform(0, 100, Correction);
-      else if (flag == 2)
-        movePlatform(-40, 0, Correction);
-      else if (flag == 3)
-        movePlatform(100, 0 , Correction);
-      lastError = error;
-    }
-  }
-  else
-  {
-    movePlatform(0, 0, 0);
-  }
-}
+	if (!junction1Enab && flag == 1 && abs(PositionR) >= movements[cur_idx].Dist)
+	{
+		Serial.println(F("Junction Check 1"));
+		junction1Enab = true;
+	}
+	if (!junction2Enab && flag != 1 && abs(PositionR) >= movements[cur_idx].Dist)
+	{
+		Serial.println(F("Junction Check 2"));
+		junction1Enab = true;
+	}
+	else {}
+	if (flag == 1/*Previous*/ && Align)
+	{
+		flag = movements[++cur_idx].Flag;
+		Align = false;
+		encodR.write(0);
+		movePlatform(0, 50, 0);
+		while (true) {
+			PositionR = abs(encodR.read());
+			Serial.print(F("Encoder align"));
+			Serial.println(PositionR);
+			if (PositionR > 25)
+				break;
+		}
+		movePlatform(0, 0, 0);
+	}
+	if (Align && flag != 1/*Previous*/)
+	{
+		flag = movements[++cur_idx].Flag;
+		Align = false;
+		movePlatform(0, 0, 0);
+	}
+	else {}
+	if (flag == 1)
+		lineValue = lineSensor1.read();
+	else
+		lineValue = lineSensor2.read();
 
-void junctionCheck()
-{
-  if (junction2Enab && flag == 1)
-  {
-    flag = 3;
-    Align = true;
-    junction2Enab = false;
-  }
+	if (lineValue >= 0 && lineValue <= 70)
+	{
+		if (micros() - pidTime > 1000) {
+			Serial.print(" Line Value: ");
+			Serial.print(lineValue);
+			pidTime = micros();
+			float error = lineValue - setPoint;
+			//      Serial.print(" Error: ");
+			//      Serial.print(error);
+			float Correction = Kp * error + Kd * (error - lastError);
+			Serial.print(" flag: ");
+			Serial.println(flag);
+			if (flag == 1)
+				movePlatform(0, 100, Correction);
+			else if (flag == 2)
+				movePlatform(-40, 0, Correction);
+			else if (flag == 3)
+				movePlatform(100, 0, Correction);
+			lastError = error;
+		}
+	}
+	else
+	{
+		movePlatform(0, 0, 0);
+	}
 }
