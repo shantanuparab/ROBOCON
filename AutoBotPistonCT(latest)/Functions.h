7303c1028a03a9680c8cf5ae71212989ca8443f0
Unknown <rahimchunara55@gmail.com>
2019-02-12 12:27:52 +0530

    11th Feb update

---
+++ AutoBotPistonCT(latest)/Functions.h
@@ -0,0 +1,187 @@
+void moveMot(char motNum, int spd) {
+  digitalWrite(motDir[motNum], spd > 0 ? LOW : HIGH);
+    spd = abs(spd);
+    spd = map(spd, 0, 255, 256, 0);
+    analogWrite(motPwn[motNum], spd);
+}
+
+void moveAllMot(int pwm0, int pwm1, int pwm2, int pwm3) {
+  moveMot(0, -pwm0);
+  moveMot(1, pwm1);
+  moveMot(2, pwm2);
+  moveMot(3, -pwm3);
+}
+
+void stopAllMot() {
+  moveMot(0, 0);
+  moveMot(1, 0);
+  moveMot(2, 0);
+  moveMot(3, 0);
+  enc0.write(0);
+  enc1.write(0);
+  enc2.write(0);
+  enc3.write(0);
+}
+
+void motorsInit() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(motPwn[i], OUTPUT);
+    pinMode(motDir[i], OUTPUT);
+    moveMot(i, 0);
+  }
+}
+
+void leg12Sync(bool dir, long countToMove) {
+  auto enc1Count = enc1.read();
+  auto enc2Count = enc2.read();
+  long error, correction, masterPwm;
+  if (dir) {
+    //Serial.print(" FORWARD ");
+    error = enc2Count - enc1Count;
+    correction = basePwm + Kp * error;
+    masterPwm = basePwm;
+    correction = constrain(correction, 0, 255);
+  }
+  else {
+    //Serial.print(" BACKWARD ");
+    error = enc2Count - enc1Count;
+    correction = -basePwm + Kp * error;
+    masterPwm = -basePwm;
+    correction = constrain(correction, -255, 0);
+  }
+  if (abs(enc1Count) > countToMove) {
+    correction = 0;
+  }
+  if (abs(enc2Count) > countToMove) {
+    masterPwm = 0;
+  }
+
+  moveAllMot(0, correction, masterPwm, 0);
+  Serial.print(" Error : ");
+  Serial.print(error);
+  Serial.print(" Correction : ");
+  Serial.print(correction);
+  Serial.print(" MasterPWM : ");
+  Serial.print(masterPwm);
+}
+
+void leg03Sync(bool dir, long countToMove) {
+  auto enc0Count = enc0.read();
+  auto enc3Count = enc3.read();
+  long error, correction, masterPwm;
+  if (dir) {
+    //Serial.print(" FORWARD ");
+    error = enc0Count - enc3Count;
+    correction = basePwm + Kp * error;
+    masterPwm = basePwm;
+    correction = constrain(correction, 0, 255);
+  }
+  else {
+    //Serial.print(" BACKWARD ");
+    error = enc0Count - enc3Count;
+    correction = -basePwm + Kp * error;
+    masterPwm = -basePwm;
+    correction = constrain(correction, -255, 0);
+  }
+  if (abs(enc0Count) > countToMove) {
+    masterPwm = 0;
+  }
+  if (abs(enc3Count) > countToMove) {
+    correction = 0;
+  }
+
+  moveAllMot(masterPwm, 0, 0, correction);
+  Serial.print(" Error : ");
+  Serial.print(error);
+  Serial.print(" Correction : ");
+  Serial.print(correction);
+  Serial.print(" MasterPWM : ");
+  Serial.print(masterPwm);
+}
+
+void motTest() {
+  Serial.println("-------All motors and encoders test-------");
+  Serial.println("Moving Motors Forward");
+
+  moveAllMot(80, 80, 80, 80);
+  delay(200);
+
+  Serial.print(enc0.read());
+  Serial.print(" ");
+  Serial.print(enc1.read());
+  Serial.print(" ");
+  Serial.print(enc2.read());
+  Serial.print(" ");
+  Serial.print(enc3.read());
+  Serial.println();
+
+  moveAllMot(-80, -80, -80, -80);
+  delay(200);
+
+  Serial.println("Moving Motors Backward");
+  Serial.print(enc0.read());
+  Serial.print(" ");
+  Serial.print(enc1.read());
+  Serial.print(" ");
+  Serial.print(enc2.read());
+  Serial.print(" ");
+  Serial.print(enc3.read());
+  Serial.println();
+
+  stopAllMot();
+}
+void moveSingleMotBack(byte motNum, int delayms) {
+  if (motNum == 1 || motNum == 2)
+    moveMot(motNum, -80);
+  if (motNum == 0 || motNum == 3)
+    moveMot(motNum, 80);
+  delay(delayms);
+  moveMot(motNum, 0);
+}
+
+void moveSingleMotForward(byte motNum, int delayms) {
+  if (motNum == 1 || motNum == 2)
+    moveMot(motNum, 80);
+  if (motNum == 0 || motNum == 3)
+    moveMot(motNum, -80);
+  delay(delayms);
+  moveMot(motNum, 0);
+}
+void singleMotTest(byte motNum) {
+  Serial.println("-------Single motor and encoder test-------");
+  Serial.println("Moving Motor Forward");
+  if (motNum == 1 || motNum == 2)
+    moveMot(motNum, 80);
+  if (motNum == 0 || motNum == 3)
+    moveMot(motNum, -80);
+  delay(200);
+
+  switch (motNum) {
+    case 0: Serial.println(enc0.read());
+      break;
+    case 1: Serial.println(enc1.read());
+      break;
+    case 2: Serial.println(enc2.read());
+      break;
+    case 3: Serial.println(enc3.read());
+      break;
+  }
+  if (motNum == 1 || motNum == 2)
+    moveMot(motNum, -80);
+  if (motNum == 0 || motNum == 3)
+    moveMot(motNum, 80);
+  delay(200);
+
+  switch (motNum) {
+    case 0: Serial.println(enc0.read());
+      break;
+    case 1: Serial.println(enc1.read());
+      break;
+    case 2: Serial.println(enc2.read());
+      break;
+    case 3: Serial.println(enc3.read());
+      break;
+  }
+
+  moveMot(motNum, 0);
+}
---
+++ AutoBotPistonCT(latest)/pins.h
@@ -0,0 +1,27 @@
+
+// pin 33 on teensy is used as reset
+#define TEENSY_RESET_PIN 33
+
+// one complete revolution of motor gives 120240 encoder readings
+#define ENCODER_REVOLVE_COUNT 120240
+
+Encoder enc0(24,25);
+Encoder enc1(29,28);
+Encoder enc2(27,26);
+Encoder enc3(31,30);
+long moveCount = 50000;
+
+const byte motPwn[] = {7,4,9,2};
+const byte motDir[] = {6,5,8,3};
+//  Front of AutoBot
+//  mot0        mot1
+//
+//
+//  mot2        mot3
+
+bool motStopped = false;
+bool syncMovDir = true;
+int selectPair = 0;
+
+float Kp = 1;
+int basePwm = 60;
