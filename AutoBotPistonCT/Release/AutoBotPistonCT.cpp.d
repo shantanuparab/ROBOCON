50b2f57f801fdb2b893f5b94bed9881fa97fa200
Pratik Chowdhury <pratikc@live.co.uk>
2019-03-26 18:23:01 +0530

    Removed Unrequired VS Files with GitIgnore added to PistonCT Directory

--- AutoBotPistonCT/Release/AutoBotPistonCT.cpp.d
+++
@@ -1,8 +0,0 @@
-C:\Users\DELL\AppData\Local\Temp\VMBuilds\AutoBotPistonCT\teensy36\Release\AutoBotPistonCT.cpp.o: \
- C:\Users\DELL\AppData\Local\Temp\VMBuilds\AutoBotPistonCT\teensy36\Release\AutoBotPistonCT.cpp \
- C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\libraries\Encoder/Encoder.h \
- C:\Users\DELL\AppData\Local\Temp\VMBuilds\AutoBotPistonCT\teensy36\Release/pch/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\libraries\Encoder/utility/direct_pin_read.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\libraries\Encoder/utility/interrupt_pins.h \
- C:\Users\DELL\AppData\Local\Temp\VMBuilds\AutoBotPistonCT\teensy36\Release\pins.h \
- C:\Users\DELL\AppData\Local\Temp\VMBuilds\AutoBotPistonCT\teensy36\Release\Functions.h
